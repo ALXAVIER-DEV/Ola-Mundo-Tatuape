f0897a95765815f38dc87d4b14bcbb4b07ec1222
Alexandre Luis Xavier <alxavier@nvquest.spo>
2020-08-27 11:36:25 -0300

    Adicionar arquivos de projeto.

---
+++ TelaPrincipal.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Ola_Mundo_Tatuape
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMensagem = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMensagem
+            // 
+            this.btnMensagem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnMensagem.Location = new System.Drawing.Point(59, 180);
+            this.btnMensagem.Name = "btnMensagem";
+            this.btnMensagem.Size = new System.Drawing.Size(75, 23);
+            this.btnMensagem.TabIndex = 0;
+            this.btnMensagem.Text = "Mensagem";
+            this.btnMensagem.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(188, 179);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnMensagem);
+            this.Name = "TelaPrincipal";
+            this.Text = "OlaMundo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMensagem;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
